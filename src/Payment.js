a34d8b8068191c6acae3f4b278a18bb980c190a0
morenikeji ajisegiri <ajisegirimorenikeji@gmail.com>
2021-02-01 17:56:29 +0100

    create stripe payment

--- src/Payment.js
+++ src/Payment.js
@@ -1,13 +1,35 @@
 import React, { Component } from 'react'
+import StripeCheckout from "react-stripe-checkout";
+import { DataContext } from "./DataContext"
 
-export class Payment extends Component {
+class Payment extends Component {
+
+   constructor(prop){
+	   super(prop);
+        this.thanks = this.thanks.bind(this);
+   }
+
+   thanks(token){
+     console.log(token)
+   }
+
+   static contextType = DataContext;
+
 	render() {
+		const { total } = this.context
 		return (
 			<div>
-				<h1>make your payment</h1>
+				<h2>Payment for items</h2>
+				<StripeCheckout 
+				 stripeKey="pk_test_51IG4hAB84gCmikSTmQdGGWuJEesi7JzVUhiYOFELqO3HEo5N10vkcaMoiBrkSJc4kWkz3rb3ouwPkt7hDseRLiVS00rBDNug8o"
+				 name="Payment for items"
+				 amount={`${total}.00`}
+				 token={this.thanks}
+				/>
 			</div>
 		)
 	}
 }
 
 export default Payment
+
