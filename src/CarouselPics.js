337aff4f8979048c961952d18cfb54bcbd2a1c26
morenikeji ajisegiri <ajisegirimorenikeji@gmail.com>
2021-03-26 15:22:20 +0100

    car

--- src/CarouselPics.js
+++ src/CarouselPics.js
@@ -5,11 +5,11 @@ function CarouselPics() {
 		<div>
 			<div id="slider">
 				<figure>
-					<img src="/bg pics/adam-jang-I7GHd8PlZqc-unsplash.jpg" className="pics" />
-					<img src="/bg pics/artiom-vallat-CHKaD8uRaDU-unsplash.jpg" className="pics" />
-					<img src="/bg pics/hermes-rivera-OX_en7CXMj4-unsplash.jpg" className="pics" />
-					<img src="/bg pics/jakob-owens-Np_nvRuhpUo-unsplash.jpg" className="pics" />
-					<img src="/bg pics/artiom-vallat-CHKaD8uRaDU-unsplash.jpg" className="pics" />
+					<img src="/bg pics/adam-jang-I7GHd8PlZqc-unsplash.jpg" alt='pic1' className="pics" />
+					<img src="/bg pics/artiom-vallat-CHKaD8uRaDU-unsplash.jpg" alt='pic2' className="pics" />
+					<img src="/bg pics/hermes-rivera-OX_en7CXMj4-unsplash.jpg" alt='pic3' className="pics" />
+					<img src="/bg pics/jakob-owens-Np_nvRuhpUo-unsplash.jpg" alt='pic4' className="pics" />
+					<img src="/bg pics/artiom-vallat-CHKaD8uRaDU-unsplash.jpg" alt='pic5' className="pics" />
 				</figure>
 			</div>
 
