9750d7b49e094c40e5fb0d4b3d760f47cab6fd01
morenikeji ajisegiri <ajisegirimorenikeji@gmail.com>
2021-04-01 18:06:58 +0100

    updated

--- src/CarouselPics.js
+++ src/CarouselPics.js
@@ -5,11 +5,21 @@ function CarouselPics() {
 		<div>
 			<div id="slider">
 				<figure>
-					<img src="/bg pics/adam-jang-I7GHd8PlZqc-unsplash.jpg" alt='pic1' className="pics" />
-					<img src="/bg pics/artiom-vallat-CHKaD8uRaDU-unsplash.jpg" alt='pic2' className="pics" />
-					<img src="/bg pics/hermes-rivera-OX_en7CXMj4-unsplash.jpg" alt='pic3' className="pics" />
-					<img src="/bg pics/jakob-owens-Np_nvRuhpUo-unsplash.jpg" alt='pic4' className="pics" />
-					<img src="/bg pics/artiom-vallat-CHKaD8uRaDU-unsplash.jpg" alt='pic5' className="pics" />
+					<div className='pics'>
+						<img src="/bg pics/adam-jang-I7GHd8PlZqc-unsplash.jpg" alt='pic1' />
+					</div>
+					<div className='pics'>
+						<img src="/bg pics/artiom-vallat-CHKaD8uRaDU-unsplash.jpg" alt='pic2' />
+					</div>
+					<div className='pics'>
+						<img src="/bg pics/hermes-rivera-OX_en7CXMj4-unsplash.jpg" alt='pic3' />
+					</div>
+					<div className='pics'>
+						<img src="/bg pics/jakob-owens-Np_nvRuhpUo-unsplash.jpg" alt='pic4' />
+					</div>
+					<div className='pics'>
+						<img src="/bg pics/artiom-vallat-CHKaD8uRaDU-unsplash.jpg" alt='pic5' />
+					</div>
 				</figure>
 			</div>
 
